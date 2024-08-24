974036ef375f9205b0859bd59bcd4fd1090ccc5a
DREAMERS-BOTS <159102147+DREAMERS-BOTS@users.noreply.github.com>
2024-08-24 22:35:17 +0530

    Update sg.py

--- DAXXMUSIC/plugins/Yumi/sg.py
+++ DAXXMUSIC/plugins/Yumi/sg.py
@@ -10,48 +10,44 @@ from DAXXMUSIC.core.userbot import assistants
 
 @app.on_message(filters.command("sg"))
 async def sg(client: Client, message: Message):
-    if len(message.text.split()) < 2 and not message.reply_to_message:
-        return await message.reply("Please provide a username, ID, or reply to a user.")
-    
+    if len(message.text.split()) < 1 and not message.reply_to_message:
+        return await message.reply("sg username/id/reply")
     if message.reply_to_message:
         args = message.reply_to_message.from_user.id
     else:
         args = message.text.split()[1]
-
     lol = await message.reply("<code>Processing...</code>")
-    
-    try:
-        user = await client.get_users(args)
-    except Exception:
-        return await lol.edit("<code>Please specify a valid user!</code>")
-    
-    bot_choice = random.choice(["sangmata_bot", "sangmata_beta_bot"])
-    
-    # Check if assistant is available
+    if args:
+        try:
+            user = await client.get_users(f"{args}")
+        except Exception:
+            return await lol.edit("<code>Please specify a valid user!</code>")
+    bo = ["sangmata_bot", "sangmata_beta_bot"]
+    sg = random.choice(bo)
     if 1 in assistants:
         ubot = us.one
-    else:
-        return await lol.edit("<code>No assistant is available.</code>")
     
     try:
-        a = await ubot.send_message(bot_choice, str(user.id))
+        a = await ubot.send_message(sg, f"{user.id}")
         await a.delete()
     except Exception as e:
-        return await lol.edit(str(e))
+        return await lol.edit(e)
+    await asyncio.sleep(1)
     
-    await asyncio.sleep(1)  # Adjust if necessary
-    
-    async for stalk in ubot.search_messages(bot_choice, limit=10):
-        if stalk and stalk.text:
-            await message.reply(stalk.text)
-            break
-    else:
-        await message.reply("No response received from the bot.")
+    async for stalk in ubot.search_messages(a.chat.id):
+        if stalk.text == None:
+            continue
+        if not stalk:
+            await message.reply("botnya ngambek")
+        elif stalk:
+            await message.reply(f"{stalk.text}")
+            break  # Exit the loop after displaying one message
     
     try:
-        user_info = await ubot.resolve_peer(bot_choice)
+        user_info = await ubot.resolve_peer(sg)
         await ubot.send(DeleteHistory(peer=user_info, max_id=0, revoke=True))
-    except Exception as e:
+    except Exception:
         pass
     
     await lol.delete()
+    
