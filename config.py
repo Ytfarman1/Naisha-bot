c56ce2a78b0f33f66bf6dc685eb39260562631db
Ytfarman1 <farmanansari9698@gmail.com>
2024-09-05 00:18:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ DEEP_API = getenv("DEEP_API")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/YTFarman9758/Naisha-bot",
+    "https://github.com/Ytfarman1/Naisha-bot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
